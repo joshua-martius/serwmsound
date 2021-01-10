cd0406eef7a7375a38ffc5a5813a778d9d33ec58
Yoshi <32636689+y0sh1DE@users.noreply.github.com>
2021-01-11 00:10:50 +0100

    Update serwmsound.sql

--- serwmsound.sql
+++ serwmsound.sql
@@ -1,33 +1,23 @@
--- phpMyAdmin SQL Dump
--- version 5.0.2
--- https://www.phpmyadmin.net/
---
--- Host: localhost
--- Erstellungszeit: 10. Jan 2021 um 18:36
--- Server-Version: 5.7.32-0ubuntu0.18.04.1
--- PHP-Version: 7.2.24-0ubuntu0.18.04.7
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
---
--- Datenbank: `serwmsound`
---
 CREATE DATABASE IF NOT EXISTS `serwmsound` DEFAULT CHARACTER SET utf16 COLLATE utf16_german2_ci;
 USE `serwmsound`;
 
--- --------------------------------------------------------
-
---
--- Tabellenstruktur für Tabelle `tblUpload`
---
+CREATE TABLE `tblComment` (
+  `cID` int(11) NOT NULL,
+  `cUploadID` varchar(16) COLLATE utf16_german2_ci NOT NULL,
+  `cAuthor` varchar(16) COLLATE utf16_german2_ci NOT NULL,
+  `cComment` text COLLATE utf16_german2_ci NOT NULL,
+  `cCreatedAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `cCreatedFrom` varchar(32) COLLATE utf16_german2_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_german2_ci;
 
 CREATE TABLE `tblUpload` (
   `uID` varchar(32) COLLATE utf16_german2_ci NOT NULL,
@@ -38,15 +28,21 @@ CREATE TABLE `tblUpload` (
   `uHits` int(11) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_german2_ci;
 
---
--- Indizes der exportierten Tabellen
---
 
---
--- Indizes für die Tabelle `tblUpload`
---
+ALTER TABLE `tblComment`
+  ADD PRIMARY KEY (`cID`),
+  ADD KEY `FK_uUpload` (`cUploadID`);
+
 ALTER TABLE `tblUpload`
   ADD PRIMARY KEY (`uID`);
+
+
+ALTER TABLE `tblComment`
+  MODIFY `cID` int(11) NOT NULL AUTO_INCREMENT;
+
+
+ALTER TABLE `tblComment`
+  ADD CONSTRAINT `FK_uUpload` FOREIGN KEY (`cUploadID`) REFERENCES `tblUpload` (`uID`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
