b0b642a9c0025f96213aceed5663b26164d6a280
y0sh1DE <Joshua.Martius@RWTH-Aachen.de>
2021-01-10 17:31:22 +0100

    Added bootstrap

--- index.php
+++ index.php
@@ -2,16 +2,36 @@
 <html>
     <head>
         <meta charset="UTF-8"/>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
         <title>serWm Sound</title>
     </head>
-    <body bgcolor="grey">
-    <center>
-        <h1>serWm Soundshare</h1>
-        <form action="upload.php" method="post" enctype="multipart/form-data">
-            Select soundfile to upload:
-            <input type="file" name="fileToUpload" id="fileToUpload"><br>
-            <input type="submit" value="Upload Soundfile" name="submit">
-        </form>
-	</center>
+    <body>
+        <div class="container">
+            <h1>serWm Soundshare</h1><hr>
+            <form action="upload.php" method="post" enctype="multipart/form-data">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">Property</th>
+                            <th scope="col">Value</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td><input type="file" name="fileToUpload" id="fileToUpload" accept=".mp3" required></td>
+                            <td><button class="btn btn-primary" type="submit" value="Upload Soundfile" name="submit">Submit Upload</button></td>
+                        </tr>
+                        <tr>
+                            <td>Interpret:</td>
+                            <td><input type="text" name="tbxInterpret" placeholder="optional"></td>
+                        </tr>
+                        <tr>
+                            <td>Song Name:</td>
+                            <td><input type="text" name="tbxSongName" placeholder="optional"></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </form>
+        </div>
     </body>
 </html>
