67869091f3f347a9da5e7ec535e586431bde1391
y0sh1DE <Joshua.Martius@RWTH-Aachen.de>
2021-01-10 15:43:56 +0100

    Added base functionality

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8"/>
+        <title>serWm Sound</title>
+    </head>
+    <body bgcolor="grey">
+    <center>
+        <h1>serWm Soundshare</h1>
+        <form action="upload.php" method="post" enctype="multipart/form-data">
+            Select soundfile to upload:
+            <input type="file" name="fileToUpload" id="fileToUpload">
+            <input type="submit" value="Upload Soundfile" name="submit">
+        </form>
+    </center>
+    </body>
+</html>
