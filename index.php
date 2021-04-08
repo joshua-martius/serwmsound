43d994cb52f67a886868c45fc8ab95705df50130
y0sh1DE <joshua.martius@rwth-aachen.de>
2021-04-08 11:27:43 +0200

    Added first raw version

--- index.php
+++ index.php
@@ -8,6 +8,9 @@
     </head>
     <body>
         <script src="./draganddrop.js"></script>
+		<?php
+			include_once "otheruploads.php";
+		?>
         <div class="drop-zone" style="position: fixed;padding: 0;margin: 0;top: 0; left: 0;width: 100%;height: 100%;">
             <div class="container">
                 <h1>serWm Soundshare</h1>
